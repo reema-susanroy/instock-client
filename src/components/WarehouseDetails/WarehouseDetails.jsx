7beef78e53a13fd70f9cc4372e70fd0ea38a1a81
Reema Roy <reemaroy73@gmail.com>
2024-03-13 14:44:58 -0700

    Implement warehouse details page and its component. Set page allignment with the background

---
+++ src/components/WarehouseDetails/WarehouseDetails.jsx
@@ -0,0 +1,44 @@
+import './WarehouseDetails.scss'
+
+import backIcon from '../../assets/icons/arrow_back-24px.svg'
+import edit from '../../assets/icons/edit-24px.svg'
+import { Link } from 'react-router-dom'
+
+function WarehouseDetails({ currentData }) {
+    return (
+        <div className='warehouse_details'>
+            <div className='warehouse_details__header-cont'>
+                <div className='warehouse_details__header'>
+                    <Link to='/warehouses' >
+                        <img className='warehouse_details__header--back-arrow' src={backIcon} alt="back-arrow" />
+                    </Link>
+                    <h1>{currentData.warehouse_name}</h1>
+                </div>
+                <div className='warehouse_details__header-edit'>
+                    <img className='warehouse_details__header--edit' src={edit} alt="edit" />
+                </div>
+            </div>
+            <div className='warehouse_details__content-cont'>
+                <div className='warehouse_details__content--address'>
+                    <h3 className='warehouse_details__content--label'>WAREHOUSE ADDRESS:</h3>
+                    <div className='warehouse_details__content--address-data'> <p>{currentData.address} ,</p>
+                        <p> {currentData.city} , {currentData.country}</p> </div>
+                </div>
+                <div className='warehouse_details__content--contact'>
+
+                    <div><h3 className='warehouse_details__content--label' >CONTACT NAME:</h3>
+                        <p>{currentData.contact_name}</p>
+                        <p>{currentData.contact_position}</p>
+                    </div>
+                    <div><h3 className='warehouse_details__content--label' >CONTACT INFORMATION:</h3>
+                        <p>{currentData.contact_phone}</p>
+                        <p>{currentData.contact_email}</p>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    )
+}
+
+export default WarehouseDetails;
