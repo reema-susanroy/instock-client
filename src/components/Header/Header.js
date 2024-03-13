0078a225b5f2f225f560bb6529eee7ea5a5daba5
serenasunyu <129626024+serenasunyu@users.noreply.github.com>
2024-03-13 09:48:26 -0400

    Add header component and make it responsive

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,24 +1,29 @@
 import "./Header.scss";
 import Logo from "../../assets/logo/InStock-Logo.svg";
 import { useNavigate } from 'react-router-dom';
+import { useState } from 'react';
 
 
 function Header() {
 
     const navigate = useNavigate();
+    const [activeButton, setActiveButton] = useState('');
 
     const goToPage = (path) => {
         navigate(path);
+        setActiveButton(path);
     };
 
     return (
         <header className="header">
             <img className="header-logo" src={Logo} alt="Logo" />
             <ul className="header-navbar">
-                {/* <li className="header-navbar__items"><a href="/warehouses">Warehouses</a></li>
-                <li className="header-navbar__items"><a href="/inventory">Inventory</a></li> */}
-                <li className="header-navbar__items"><button onClick={() => goToPage('/warehouses')}>Warehouses</button></li>
-                <li className="header-navbar__items"><button onClick={() => goToPage('/inventory')}>Inventory</button></li>
+                <li className="header-navbar__items">
+                    <button className={`nav-button ${activeButton === '/warehouses' ? 'active' : ''}`} onClick={() => goToPage('/warehouses')}>Warehouses</button>
+                </li>
+                <li className="header-navbar__items">
+                    <button className={`nav-button ${activeButton === '/inventory' ? 'active' : ''}`} onClick={() => goToPage('/inventory')}>Inventory</button>
+                </li>
             </ul>
         </header>
     )
