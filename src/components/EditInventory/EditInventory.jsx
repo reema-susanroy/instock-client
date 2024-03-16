c1ee01255963d604196ec6757bf58ba67b21f0a2
Reema Roy <reemaroy73@gmail.com>
2024-03-16 10:23:45 -0700

    Implement logic to do an update on DB when the Warehouse is changed for an item

--- src/components/EditInventory/EditInventory.jsx
+++ src/components/EditInventory/EditInventory.jsx
@@ -88,15 +88,38 @@ function EditInventory({ inventory, warehouseName, warehouseId , thisPath}) {
         return true;
     }
 
+    const warehouseObj = {
+        "1" : "Manhattan",
+        "2" : "Washington",
+        "3": "Jersey",
+        "4": "SF",
+        "5":"Santa Monica",
+        "6": "Seattle",
+        "7" : "Miami",
+        "8" : "Boston"
+    }
+    const checkWarehouseSelected =() =>{
+        if(selectedWarehouse !== warehouseName){
+            for (const [id, warehouse] of Object.entries(warehouseObj)) {
+                if (warehouse === selectedWarehouse) {
+                    return id;
+                }
+            }
+        }
+        return warehouseId;
+    }
+
     const updateWarehouse = async (e) => {
         e.preventDefault();
         const Validation = validateInput();
         const validateQuantity = validateQuantities();
+        const checkWarehouseUpdateId = checkWarehouseSelected();
+
         if (Validation && validateQuantity) {
             setUpdateSuccess(false);
             try {
                 await axios.put(`http://localhost:5000/api/inventories/${inventory.id}`, {
-                    warehouse_id: warehouseId,
+                    warehouse_id: checkWarehouseUpdateId,
                     item_name: itemName,
                     description: description,
                     category: selectedCategory,
